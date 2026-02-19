0ae567ff99471dda37ac9115b6d6c473f5fd3ff9
jamesborder <me@jamesborder.com>
2026-02-19 10:28:51 -0600

    Initial Commit

---
+++ MapAccess/MapAccessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAccessApp.swift
+//  MapAccess
+//
+//  Created by James Border on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct MapAccessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapAccessTests/MapAccessTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapAccessTests.swift
+//  MapAccessTests
+//
+//  Created by James Border on 2/19/26.
+//
+
+import Testing
+@testable import MapAccess
+
+struct MapAccessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
